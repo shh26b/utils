ddb3d1578611b0d9a6dda83db6571bb3ebacf5b7
Shihab Mahamud <shihab4t@gmail.com>
2022-12-09 20:47:06 +0600

    Rename file with rejex

---
+++ py/rename_file_with_rejex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from sys import argv
+import re
+import shutil
+
+
+def format_name(file: str) -> str:
+    file = re.sub(" ", "_", file)
+    file = re.sub("-", "_", file)
+    i = file.find(".")
+    return file[: i] + "_" + file[i + 2:]
+
+
+def mv_file(src: str, dst: str) -> None:
+    shutil.move(src, dst)
+
+
+if __name__ == "__main__":
+    name = argv[1]
+    formatted_name = format_name(name)
+    mv_file(name, formatted_name)
