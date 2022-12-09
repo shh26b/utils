ced917eca253c6661d20c49b1f80ea295c97c347
Shihab Mahamud <67628903+shh26b@users.noreply.github.com>
2022-12-09 22:00:25 +0600

    Update rename_file_with_rejex.py

--- py/rename_file_with_rejex.py
+++ py/rename_file_with_rejex.py
@@ -3,6 +3,7 @@
 from sys import argv
 import re
 import shutil
+import os
 
 
 def format_name(file: str) -> str:
@@ -10,11 +11,16 @@ def format_name(file: str) -> str:
     file = re.sub("-", "_", file)
     return file
 
+
 def mv_file(src: str, dst: str) -> None:
     shutil.move(src, dst)
 
 
 if __name__ == "__main__":
-    name = argv[1]
-    formatted_name = format_name(name)
-    mv_file(name, formatted_name)
+    rootdir = '/home/shh26b/src/dsa/stl'
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            formatted_file = format_name(file)
+            mv_file(os.path.join(subdir, file),
+                    os.path.join(subdir, formatted_file))
+            print(os.path.join(subdir, formatted_file))
